0f9d25cd06f4bd3c3a9c0da507c08fce74f7b929
Andrea <andrea.mercuri69@gmail.com>
2020-06-24 12:44:56 +0200

    last

---
+++ OANDA/Model/Communication/IEX/IEXstocksListResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OANDA.Model.Communication
+{
+   public  class IEXstocksListResponse
+    {
+        public data[] data { get; set; }
+       public pagination pagination { get; set; }
+    }
+
+    public class data
+    {
+        public string name{ get; set; }
+        public string symbol{ get; set; }
+
+    }
+
+    public class pagination  {
+        public string limit { get; set; }
+        public string offset { get; set; }
+        public string count { get; set; }
+        public string total { get; set; }
+    }
+
+
+}
---
+++ OANDA/Model/Stock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OANDA.Model
+{
+   public class Stock
+    {
+        public string name { get; set; }
+        public string symbol { get; set; }
+
+    }
+}
