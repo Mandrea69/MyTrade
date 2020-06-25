11a9d53181d684f57535336ad27f10ca52e00de6
Andrea Mercuri <andrea.mercuri69@gmail.com>
2020-06-25 22:43:28 +0100

    commit

--- OANDA/Program.cs
+++ OANDA/Program.cs
@@ -13,6 +13,8 @@ using System.Text.Json;
 using OANDA.Model.Communication;
 using OANDA.Data;
 using System.Runtime.CompilerServices;
+using System.Globalization;
+using OANDA.Utilities;
 
 namespace OANDA
 {
@@ -25,8 +27,8 @@ namespace OANDA
         {
 
 
-        
-           Console.WriteLine("1 Indeces and Currency - Daily");
+
+            Console.WriteLine("1 Indeces and Currency - Daily");
             Console.WriteLine("2 Indeces and Currency - Weekly");
             Console.WriteLine("3 Calcolate Units");
             string selectedOption = Console.ReadLine();
@@ -35,7 +37,7 @@ namespace OANDA
                 HA_IndicesAndCurrency.Run();
 
             }
-            else if(selectedOption == "2")
+            else if (selectedOption == "2")
             {
                 HA_IndicesAndCurrencyW.Run();
 
@@ -43,13 +45,14 @@ namespace OANDA
             else if (selectedOption == "3")
             {
                 Console.WriteLine("Insert Instrument");
-                string instrument = Console.ReadLine();
+               
+                string instrument = AutoComplete.Run();
                 Console.WriteLine("Insert stop loss");
                 double sl = Convert.ToDouble(Console.ReadLine());
                 double currentPrice = Data.Prices.LastPrice(instrument);
                 Utilities.Calculate.Units units = new Utilities.Calculate.Units(instrument, 100, 1, sl);
                 Console.WriteLine(units.Get());
-               
+
 
             }
             else
@@ -57,9 +60,10 @@ namespace OANDA
 
                 HA_Stocks.Run();
             }
-
-          
+           
 
         }
-}
+    }
+
+
 }
---
+++ OANDA/Utilities/AutoComplete.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace OANDA.Utilities
+{
+    public class AutoComplete
+    {
+        public static string Run()
+        {
+
+            List<Model.Instrument> instruments = Data.Instrument.AllFromFile().OrderBy(x => x.Name).ToList();
+            var builder = new StringBuilder();
+            var input = Console.ReadKey(intercept: true);
+            int i = 0;
+            Model.Instrument match = null;
+            while (input.Key != ConsoleKey.Enter)
+            {
+                var currentInput = builder.ToString();
+                if (input.Key == ConsoleKey.Tab)
+                {
+                  
+                    if (i == 0)
+                    {
+                        match = instruments.FirstOrDefault(item => item.Name != currentInput && item.Name.StartsWith(currentInput, true, CultureInfo.InvariantCulture));
+                        for (int y = 0; y < instruments.Count; y++)
+                        {
+                            if (instruments[y].Name == match.Name)
+                            {
+
+                                i = y;
+                            }
+                        }
+                    }
+
+
+                    else
+                        match = instruments[i];
+
+                    if (string.IsNullOrEmpty(match.Name))
+                    {
+                        input = Console.ReadKey(intercept: true);
+                        continue;
+                    }
+
+                    ClearCurrentLine();
+                    builder.Clear();
+
+                    Console.Write(match.Name);
+                    builder.Append(match.Name);
+                    i += 1;
+                }
+                else
+                {
+                    if (input.Key == ConsoleKey.Backspace && currentInput.Length > 0)
+                    {
+                        builder.Remove(builder.Length - 1, 1);
+                        ClearCurrentLine();
+
+                        currentInput = currentInput.Remove(currentInput.Length - 1);
+                        Console.Write(currentInput);
+                    }
+                    else
+                    {
+                        i = 0;
+                        var key = input.KeyChar;
+                        builder.Append(key);
+                        Console.Write(key);
+                    }
+                }
+
+                input = Console.ReadKey(intercept: true);
+            }
+           
+            Console.Write(input.KeyChar);
+            Console.WriteLine(match.Name);
+            return match.Name;
+        }
+
+        /// <remarks>
+        /// https://stackoverflow.com/a/8946847/1188513
+        /// </remarks>>
+        private static void ClearCurrentLine()
+        {
+            var currentLine = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLine);
+        }
+
+    }
+}
+
